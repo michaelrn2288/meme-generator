f6c95afc3c69a1f65d1406d1c7680a1a18ddc654
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-10-29 23:38:25 -0300

    first commit

---
+++ meme-generator/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Header () {
+
+    return (
+        <header className="header">
+            <img
+                src="./images/troll-face.png"
+                className="header--image"
+            />
+
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React project</h4>
+
+        </header>
+    )
+}
